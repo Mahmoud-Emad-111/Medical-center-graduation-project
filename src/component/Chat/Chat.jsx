b13f763be7e8b4df0b52ff44ee83211ff1c23836
Mahmoud-Emad-111 <mahmoud.devops@gmail.com>
2024-05-11 14:25:50 +0300

    Pusher v1

--- src/component/Chat/Chat.jsx
+++ src/component/Chat/Chat.jsx
@@ -4,8 +4,10 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router";
 import Cookies from 'react-cookies'
 import http from '../http';
+import Pusher from 'pusher-js';
 
 export default function Chat() {
+  const [Message, setMessage] = useState([]);
   const navigate = useNavigate();
   const {id} =useParams();
   const [DoctorChat, setDoctorChat] = useState([]);
@@ -19,37 +21,87 @@ export default function Chat() {
         http.defaults.headers.common = {'Authorization': `Bearer ${token}`}
 
         http.post('api/Chat/Get_Chat_Doctor',{
-          doctor_id:1,
+          doctor_id:id,
         }).then(res=>{
             
-          setDoctorChat(res.data);
+          setDoctorChat(res.data)
         })
         
     }
     
-}, []);  return (
+}, []);
+
+function ScrollToBottom(){
+  var d = document.querySelector('.body_chat');
+  // console.log(d);
+  // d.scrollTop = d.scrollHeight;
+}
+ScrollToBottom()
+
+  Pusher.logToConsole = false;
+
+  var pusher = new Pusher('3fc9bba59aceb10daa36', {
+    cluster: 'eu'
+  });
+  
+  var channel = pusher.subscribe('channel');
+  channel.bind('event', function(data) {
+    
+    setDoctorChat([...DoctorChat,data.data]);
+    console.log('done');
+  });
+
+
+  function Send_message(e){
+    e.preventDefault();
+
+    const token=Cookies.load('token');
+
+    http.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+
+    http.post('api/Chat/User_Message',{
+      doctor_id:id,
+      message :Message,
+
+    }).then(res=>{
+        
+      if (res.status===200) {
+          setMessage('')
+
+      }
+    })
+  
+  }
+
+return (
     <div className='container mt-5 py-4 rounded-1'>
         <div className='body_chat'>
         <div className='text'>
           <div className='item_chat'>
-          <span  className='receiver'>Text</span>
-
-          </div>
-          <div className='item_chat'>
-            {
+          
+          {
               DoctorChat.map((e)=>{
-                 
-                  return   <span className='sender'>{e.message}</span>
-                   
+
+                  
+                    return e.sender=='User' ?    <div  className='receiver'>{e.message}</div> : <div  className='sender'>{e.message}</div>;
                   })
                 }
-            </div>
+
+          </div>
+
         </div>
         </div>
-        <div className='input_chat'>
-            <input type="text" name="" id="" />
-            <button type='submit'>Sent</button>
+        <form onSubmit={Send_message}>
+        <div className='input_chat' >
+            <input type="text" name="Message" value={Message} id=""   onChange={(e)=>setMessage(e.target.value)}/>
+            <button type='submit' >Sent</button>
         </div>
+
+        </form>
     </div>
   )
 }
+
+
+
+
